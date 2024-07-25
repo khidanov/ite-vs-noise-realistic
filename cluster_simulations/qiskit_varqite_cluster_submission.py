509ccbeddc0cb10ac063cb4a896957f3e3ce332a
Aleksei Khindanov <khindalex@gmail.com>
2024-07-25 17:42:44 -0500

    relocating files

---
+++ cluster_simulations/qiskit_varqite_cluster_submission.py
@@ -0,0 +1,114 @@
+"""
+This script generates job scripts to run "qiskit_varqite_wrapper.py" on
+multiple computing nodes on a cluster.
+The parameters for "qiskit_varqite_wrapper.py" are specified in the
+"set_of_params" list below.
+Separate job is created and executed for each parameter set.
+"""
+
+import numpy as np
+import os
+
+"""
+Specifying parameter values for "qiskit_varqite_wrapper.py"
+"""
+set_of_params=[(num_qubits,
+                round(g,3),
+                time,
+                estimator_type,
+                noise_type,
+                p,
+                estimator_approximation,
+                estimator_method)
+                for num_qubits in [8,10,12]
+                for g in np.linspace(0.1,2.0,20)
+                for time in [5.0]
+                for estimator_type in ["noisy"]
+                for noise_type in ["X"]
+                for p in [0.0]
+                for estimator_approximation in [True]
+                for estimator_method in ['density_matrix']
+                ]
+
+# set_of_params=[(num_qubits,
+#                 round(g,3),
+#                 time,
+#                 estimator_type,
+#                 noise_type,
+#                 p,
+#                 estimator_approximation,
+#                 estimator_method)
+#                 for num_qubits in [8,10,12]
+#                 for g in np.linspace(0.1,2.0,20)
+#                 for time in [5.0]
+#                 for estimator_type in ["noiseless"]
+#                 for noise_type in [None]
+#                 for p in [None]
+#                 for estimator_approximation in [None]
+#                 for estimator_method in [None]
+#                 ]
+
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+
+"""
+For each parameter value, creating an sbatch job to execute.
+Execution logs are saved into a directory logs/.
+"""
+CPUs = 24
+for params in set_of_params:
+    (num_qubits,
+    g,
+    time,
+    estimator_type,
+    noise_type,
+    p,
+    estimator_approximation,
+    estimator_method) = params
+    job_file = "qiskit_varqite_loop_submission_job.sbatch"
+    with open(job_file, "w") as fh:
+        fh.writelines("#!/bin/bash\n")
+        fh.writelines("#SBATCH -t 20:00:00\n")
+        fh.writelines("#SBATCH --nodes=1\n")
+        fh.writelines("#SBATCH --ntasks-per-node=1\n") #
+        fh.writelines("#SBATCH --cpus-per-task="+str(CPUs)+"\n")
+        fh.writelines("#SBATCH --mem-per-cpu=4G\n")
+        fh.writelines("#SBATCH --partition=dense\n")
+        fh.writelines("#SBATCH --hint=compute_bound\n")
+        fh.writelines("#SBATCH -o " + file_dir + "/qiskit_varqite_out\n")
+        fh.writelines("#SBATCH -e " + file_dir + "/qiskit_varqite_err\n")
+        fh.writelines("#SBATCH --job-name=\"L"+str(num_qubits)+"g"+str(g)+
+                        "p"+str(p)+"\"\n")
+        # fh.writelines("#SBATCH --mail-user=akhin@iastate.edu\n")
+        # fh.writelines("#SBATCH --mail-type=FAIL\n")
+        fh.writelines("export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE\n")
+        fh.writelines("export OPENBLAS_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE\n")
+        fh.writelines("export MKL_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE\n")
+        # fh.writelines("module load python py-numpy py-scipy py-joblib"
+        #     "ml-gpu/20230427\n")
+        fh.writelines("{ time python " +
+                        file_dir +
+                        ("/qiskit_varqite_run.py "
+                        "-n %s "
+                        "-g %s "
+                        "-t %s "
+                        "--estimator_type %s "
+                        "--noise_type %s "
+                        "-p %s "
+                        "--estimator_approximation %s "
+                        "--estimator_method %s "
+                        "--num_cpus %s ")
+                        % (params + (CPUs,)) +
+                        (" ; } 2> logs/time_qiskit_varqite_L%s_g%s_time%s_%s_"
+                        "noise%s_p%s_approx%s_%s.txt \n") % params
+                    )
+
+    """
+    Executing the job.
+    """
+    os.system("sbatch %s" %job_file)
+
+"""
+Deleting the .sbatch file in the end.
+"""
+os.remove(job_file)
---
+++ qiskit_varqite_run.py
@@ -0,0 +1,173 @@
+"""
+This script utilizes module "qiskit_varqite_main_class.py" to
+perform VarQITE and ITE simulations for the TFIM using Qiskit.
+
+Running "python qiskit_varqite_run.py" would run the code on the head node with
+default parameters (see below).
+Otherwise, the parameters can be specified in the command line or in the job
+generating script to run the code on computing nodes
+(see "qiskit_varqite_cluster_submission.py").
+"""
+
+import argparse
+import os
+import pickle
+import sys
+
+import qiskit_varqite_main_class
+from qiskit_varqite_main_class import Noisy_varqite_qiskit_tfim
+
+def none_or_str(value):
+    """
+    Function used to assign the type None to a command line variable if the
+    command line reads "None", str otherwise.
+    """
+    if value == 'None':
+        return None
+    return value
+
+def none_or_float(value):
+    """
+    Function used to assign the type None to a command line variable if the
+    command line reads "None", float otherwise.
+    """
+    if value == 'None':
+        return None
+    return float(value)
+
+def none_or_bool(value):
+    """
+    Function used to assign the type None to a command line variable if the
+    command line reads "None", bool otherwise.
+    """
+    if value == 'None':
+        return None
+    return bool(value)
+
+"""
+Reading out parameters from the command line.
+"""
+parser = argparse.ArgumentParser(
+    description = "Perform VarQITE and ITE simulations for TFIM using Qiskit"
+)
+parser.add_argument(
+    "-n",
+    "--num_qubits",
+    type=int,
+    default=2,
+    metavar='\b',
+    help="number of qubits"
+)
+parser.add_argument(
+    "-g",
+    "--g",
+    type=float,
+    default=0.1,
+    metavar='\b',
+    help="transverse field"
+)
+parser.add_argument(
+    "-t",
+    "--time",
+    type=float,
+    default=5.0,
+    metavar='\b',
+    help="total simulation (imaginary) time"
+)
+parser.add_argument(
+    "--estimator_type",
+    type=str,
+    choices=["noiseless", "noisy"],
+    default="noiseless",
+    metavar='\b',
+    help="estimator type"
+)
+parser.add_argument(
+    "--noise_type",
+    type=none_or_str,
+    choices=[None, "X", "depolarizing"],
+    default=None,
+    help="noise type"
+)
+parser.add_argument(
+    "-p",
+    "--p",
+    type=none_or_float,
+    default=None,
+    metavar='\b',
+    help="noise probability"
+)
+parser.add_argument(
+    "--estimator_approximation",
+    type=none_or_bool,
+    choices=[None, True, False],
+    default=None,
+    metavar='\b',
+    help="estimator approximation flag"
+)
+parser.add_argument(
+    "--estimator_method",
+    type=none_or_str,
+    choices=[None, "density_matrix", "statevector"],
+    default=None,
+    metavar='\b',
+    help="estimator method"
+)
+parser.add_argument(
+    "--num_cpus",
+    type=int,
+    default=1,
+    metavar='\b',
+    help="number of CPUs"
+)
+args = parser.parse_args()
+
+"""
+Initializing an instance of the class.
+"""
+varqite_obj = Noisy_varqite_qiskit_tfim(args.num_qubits)
+
+"""
+Performing VarQITE.
+"""
+evolution_result_varqite = varqite_obj.qiskit_varqite(
+    args.g,
+    args.time,
+    args.estimator_type,
+    args.noise_type,
+    args.p,
+    args.estimator_approximation,
+    args.estimator_method
+)
+
+"""
+Performing ITE.
+"""
+time_step=0.01
+evolution_result_ite = varqite_obj.qiskit_ite(
+    args.g,
+    args.time,
+    time_step
+)
+
+evolution_result = dict()
+evolution_result["varqite"] = evolution_result_varqite
+evolution_result["ite"] = evolution_result_ite
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+
+"""
+Saving evolution_result dictionary containing desired objects to a file.
+"""
+with open(file_dir +
+        '/data/qiskit_varqite_L%s_g%s_time%s_%s_noise%s_p%s_approx%s_%s.pkl' % (
+            args.num_qubits,
+            args.g,
+            args.time,
+            args.estimator_type,
+            args.noise_type,
+            args.p,
+            args.estimator_approximation,
+            args.estimator_method
+            ), 'wb') as outp:
+    pickle.dump(evolution_result, outp, pickle.HIGHEST_PROTOCOL)
